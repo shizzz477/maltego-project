7a9f4e4b494a439a96a50dc4402b0186f9847a6c
tab262 <caseface31@gmail.com>
2013-11-23 10:57:44 -0500

    Create emailScraper.py
    Simple python script for scraping emails from html and writing to CSV file. Can import the CSV file to Maltego.
    
    Can change the URL to change the web page. Can later make the url a command line arguement.

---
+++ emailScraper.py
@@ -0,0 +1,32 @@
+import urllib
+import os
+import re
+import sys
+import csv
+
+#stolen from http://love-python.blogspot.com/2008/04/python-code-to-scrape-email-address.html
+def collectAllEmail(htmlSource):
+        "collects all possible email addresses from a string, but still it can miss some addresses"
+        #example: t.s@d.com
+        email_pattern = re.compile("[-a-zA-Z0-9._]+@[-a-zA-Z0-9_]+.[a-zA-Z0-9_.]+")
+        emails = re.findall(email_pattern, htmlSource)
+        return emails
+
+def collectEmail( htmlSource):
+    "collects all emails that starts with mailto: in the html source string"
+    #example: <a href="mailto:t.s@d.com">
+    email_pattern = re.compile("<a\s+href=\"mailto:([a-zA-Z0-9._@]*)\">", re.IGNORECASE)
+    emails = re.findall(email_pattern, htmlSource)
+    return emails
+
+url = "http://www.bu.edu/cs/people/directory/";
+
+html = urllib.urlopen(url).read()
+
+emails = collectAllEmail(html) 
+
+print emails
+
+myfile = open('emails.csv', 'wb')
+wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+wr.writerow(emails)

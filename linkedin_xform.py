e62c5e3b77f34f5b13db4733a8e93a9864d13702
jancojt <jancojt@gmail.com>
2013-11-23 15:57:28 -0500

    courtney commit

---
+++ linkedin_xform.py
@@ -0,0 +1,27 @@
+import urllib
+import os
+import re
+import sys
+import csv
+from MaltegoTransform import *
+
+def LinkedIn(linkedinusername):
+	from pygoogle import pygoogle
+	g = pygoogle("linkedin "+linkedinusername)
+	g.pages = 5
+	g.get_result_count()
+	myURLs = g.get_urls()
+	return myURLs
+
+
+def main(argv):
+	myURLs = LinkedIn(sys.argv[1])
+
+	mt = MaltegoTransform();
+	for urls in myURLs:
+		mt.addEntity("maltego.Alias", urls)
+
+	mt.returnOutput()
+
+
+	if __name__ == "__main__": main(sys.argv)

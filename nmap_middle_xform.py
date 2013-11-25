61196ded390a6405f8e0bec623f9d7575f9d0e6d
jancojt <jancojt@gmail.com>
2013-11-25 16:51:05 -0500

    cleaned up nmap, added fields

--- nmap_middle_xform.py
+++ nmap_middle_xform.py
@@ -4,50 +4,49 @@
 # NMAP SCAN TOOL#
 #################
 
-import os, sys
+import os, sys, re
 from MaltegoTransform import *
 
 m_ent = MaltegoTransform();
 m_ent.parseArguments(sys.argv);
 
-ports = "20-25,80,6789" # ports of hosted HTTP index.html
-victim = sys.argv[1] # ip address of victim 
+ports = "20-25,80,6789"
+victim = sys.argv[1] # ip address of victim  
+file_name = victim
 
-file_name = victim;
+nmap_cmd_line_args = "nmap -oG " + file_name + " -sV -p" + ports + " " + victim + ">" + file_name + ".txt"
 
-# run nmap and pipe to cat
-nmap_cmd_line_args = "nmap -oG " + file_name + " -sV -p" + ports + " " + victim + ">" + file_name + ".txt" # content not allowed in xml prolog...out here
-os.system("rm " + file_name + ".txt")
-os.system(nmap_cmd_line_args); 
+os.system(nmap_cmd_line_args)
 
 # open output file of nmap
 f = open(file_name)
 for line in f:
-    fields = line.split("\t")
+    cut_host = re.sub(r'Host(.*)\t', '', line)
+    if cut_host[:5] == "Ports":
+        cut_ports = re.sub(r'Ports: ', '', cut_host)
+        fields = cut_ports.split(", ")
+        for field in fields:
+            kill_slash = field.replace("//", "/")
+            port_vals = kill_slash.split("/")
 
-    # port_dump now gives each port output
-    for field in fields:
-        the_field = field.split(": ");
-        if (the_field[0] == "Ports"):
-            port_dump = the_field[1].split(",");
+            port_num = port_vals[0].strip()
+            is_open = port_vals[1].strip()
+            protocol = port_vals[2].strip()
+            service = port_vals[3].strip()
+            banner = port_vals[4].strip()
 
-            # go through port_dump and pick out each value
-            for port in port_dump:
-                port = port.replace("//", "/");
-                port_vals = port.split("/");
+            if is_open == "closed":
+                service = "N/A"
 
-                # parse the values block and put into relevant entities
-                port_num = port_vals[0].strip();
-                is_open = port_vals[1].strip();
-                service = port_vals[4].strip();
-
-                if (is_open == "closed"):
-                    service = "N/A"
-
-                # show them in maltego ui
-                me_serv = m_ent.addEntity("Scanned_Ports", "PORT: " + port_num + " is " + is_open + "\n"  + " SERVICE: " + service);
+            # show them in maltego ui
+            me_serv = m_ent.addEntity("Scanned_Ports", "PORT: " + port_num + " is " + is_open + "\n"  + " SERVICE: " + service);
+            me_serv.addAdditionalFields("port", "Port", None, port_num) 
+            me_serv.addAdditionalFields("protocol", "Protocol", None, protocol)
+            me_serv.addAdditionalFields("service",  "Service", None, service)
+            me_serv.addAdditionalFields("banner", "Banner", None, banner)
+            me_serv = None
 
 f.close();
 os.system("rm " + file_name)
+os.system("rm " + file_name + ".txt")
 m_ent.returnOutput();
-

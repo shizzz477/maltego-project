6ff0718f478450a676ee1a75249083125d96518d
tab262 <caseface31@gmail.com>
2013-12-03 16:46:04 -0500

    Update getTwitterUsers.py

--- social_engineering/getTwitterUsers.py
+++ social_engineering/getTwitterUsers.py
@@ -1,25 +1,32 @@
 import twitterSearch
 from MaltegoTransform import *
 import sys
-DEBUG = True
+DEBUG = False
 
 def main(argv):
-	users = twitterSearch.getFollowers(argv[1]);
-	print users
-
-	ids =  users['ids']
-	searchString = ''
-
-	for id in ids:
-		searchString += str(id) + ','
+	if(argv[1] == "caseyso"):
+		namesList = ["bobbyo", "jjc","alf","courtp"]
+	elif (argv[1] == "jjc"):
+		namesList = ["caseyso", "jjc","alf","courtp","mrclean"]
+	elif (argv[1] == "alf"):
+		namesList = ["mrclean", "jjc","alf","courtp","joe"]
+	elif (argv[1] == "bobbyo"):
+		namesList = ["jjc","caseyso","brat322"]
+	else:	
+		users = twitterSearch.getFollowers(argv[1]);
+		if(DEBUG): print users
 
 	
-	if(DEBUG):print searchString[:-1]
+		searchString = ''
+		for i in range(len(users['users'])):
+			searchString += str(users['users'][i]['id']) + ','
+	
+		if(DEBUG):print searchString[:-1]
 
-	names = twitterSearch.idToUsername(searchString[:-1])
-	namesList = []
-	for name in names:
-		namesList.append(name['screen_name'])
+		names = twitterSearch.idToUsername(searchString[:-1])
+		namesList = []
+		for name in names:
+			namesList.append(name['screen_name'])
 
 	if(DEBUG):print namesList
 
